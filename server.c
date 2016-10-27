818426d752a80bb9afcf7d811f5be8bb26a82c47
jigyasaPopat <jigyasapopat@raveendra-OptiPlex-9020.(none)>
2016-10-27 14:56:22 +0530

    server and client

---
+++ server.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<stdlib.h>
+int main(int argc,char *argv[])
+{
+
+/*variables*/
+
+int sock;
+struct sockaddr_in server;
+int mysock;
+char buff[1024];
+int rval;
+
+/*create socket*/
+
+sock=socket(AF_INET,SOCK_STREAM,0);
+if(sock<0)
+{
+perror("failed to create socket");
+exit(1);
+}
+
+server.sin_family=AF_INET;
+server.sin_addr.s_addr=INADDR_ANY;
+server.sin_port=htons(5000);
+
+/*call bind*/
+
+if(bind(sock,(struct sockaddr *)&server,sizeof(server)))
+{
+perror("bind failed");
+exit(1);
+}
+
+/*listen*/
+
+listen(sock,5);
+
+/*accept*/
+
+do
+{
+mysock=accept(sock,(struct sockaddr *)0,0);
+
+if(mysock==-1)
+{ 
+perror("accept failed");
+}
+else
+{
+memset(buff,0,sizeof(buff));
+if((rval=recv(mysock,buff,sizeof(buff),0))<0)
+{ 
+perror("reading stream message error");
+}
+else if(rval==0)
+{ 
+printf("ending connection\n");
+}
+else
+{
+printf("msg=%s\n",buff);
+}
+printf("got the message (rval=%d)\n",rval);
+close(mysock);
+}
+}while(1);
+return 0;
+}
